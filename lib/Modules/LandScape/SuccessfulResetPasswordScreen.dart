55633c321c2dd0a09526b36c203b6b634f363768
Abdelaziz66 <3bdel3zizelsayed123@gmail.com>
2024-02-15 11:11:32 +0200

    new commit

--- lib/Modules/LandScape/SuccessfulResetPasswordScreen.dart
+++ lib/Modules/LandScape/SuccessfulResetPasswordScreen.dart
@@ -7,20 +7,21 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:university_hup/Layout/LayoutScreen.dart';
 import 'package:university_hup/Modules/LandScape/ForgetPasswordScreen.dart';
+import 'package:university_hup/Modules/LandScape/LoginScreen.dart';
 import 'package:university_hup/Shared/Cons_widget.dart';
 import 'package:university_hup/Shared/Cubit/App_cubit.dart';
 import 'package:university_hup/Shared/Cubit/App_state.dart';
 import 'package:university_hup/Shared/constant.dart';
 
 
-class loginscreen extends StatefulWidget {
-  const loginscreen({Key? key}) : super(key: key);
+class SuccessfulResetPasswordScreen extends StatefulWidget {
+  const SuccessfulResetPasswordScreen({Key? key}) : super(key: key);
 
   @override
-  State<loginscreen> createState() => _loginscreenState();
+  State<SuccessfulResetPasswordScreen> createState() => _loginscreenState();
 }
 
-class _loginscreenState extends State<loginscreen> {
+class _loginscreenState extends State<SuccessfulResetPasswordScreen> {
   var emailcontroller = TextEditingController();
   var passwordcontroller = TextEditingController();
   var formkey = GlobalKey<FormState>();
@@ -108,7 +109,7 @@ class _loginscreenState extends State<loginscreen> {
                                   child: Padding(
                                     padding: const EdgeInsets.all(20.0),
                                     child: Image.asset(
-                                      'assets/images/login.png',
+                                      'assets/images/finishresetpassword.png',
                                     ),
                                   ),
                                 ),
@@ -117,7 +118,7 @@ class _loginscreenState extends State<loginscreen> {
                                 height: 25,
                               ),
                               Text(
-                                  'Welcome !',
+                                  'Password Changed',textAlign: TextAlign.center,
                                   style: Theme.of(context).textTheme.bodyText1?.copyWith(
                                       fontSize: 35
                                   )
@@ -162,7 +163,7 @@ class _loginscreenState extends State<loginscreen> {
                               //       ]),
                               // ),
                               Text(
-                                  'You can login here',
+                                  'Your Password has been successfully change and you can now begin your user experience',textAlign: TextAlign.center,
                                   style: Theme.of(context).textTheme.subtitle1?.copyWith(
                                       fontSize: 25
                                   )
@@ -174,164 +175,26 @@ class _loginscreenState extends State<loginscreen> {
                               const SizedBox(
                                 height: 30,
                               ),
-                              Container(
-                                alignment: Alignment.center,
-                                height: 70,
-                                decoration: BoxDecoration(
-                                  // border: Border.all(color: Colors.white),
-                                  borderRadius: BorderRadius.circular(18),
-                                  color: Colors.blueGrey.withOpacity(.1),
-                                ),
-                                child: Padding(
-                                  padding:
-                                  const EdgeInsets.symmetric(horizontal: 8.0),
-                                  child: TextFormField(
-                                    controller: emailcontroller,
-                                    keyboardType: TextInputType.emailAddress,
-                                    onFieldSubmitted: (value) {
-                                      print(value);
-                                    },
-                                    onChanged: (value) {
-                                      print(value);
-                                    },
-                                    validator: (value) {
-                                      if (value!.isEmpty) {
-                                        return 'Email can\'t be empty';
-                                      }
-                                      return null;
-                                    },
-                                    // toolbarOptions:
-                                    //     ToolbarOptions(paste: true, copy: true),
-                                    cursorColor: c1,
-                                    style: const TextStyle(
-                                      fontSize: 25,
-                                    ),
-                                    decoration: InputDecoration(
-                                      prefixIcon: Icon(
-                                        Icons.email,
-                                        color: c1,
-                                        size: 30,
-                                      ),
-                                      hintText: 'email',
-                                      border: InputBorder.none,
-                                    ),
-                                  ),
-                                ),
-                              ),
+
+
                               const SizedBox(
                                 height: 15,
-                              ),
-                              Container(
-                                alignment: Alignment.center,
-                                height: 70,
-                                decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.white),
-                                  borderRadius: BorderRadius.circular(18),
-                                  color:Colors.blueGrey.withOpacity(0.1),
-                                  //HexColor('D8E1E8FF'),
-                                ),
-                                child: Padding(
-                                  padding:
-                                  const EdgeInsets.symmetric(horizontal: 8.0),
-                                  child: TextFormField(
-                                    controller: passwordcontroller,
-                                    keyboardType: TextInputType.visiblePassword,
-                                    obscureText: passwordcheck ? false : true,
-                                    onFieldSubmitted: (value) {
-                                      print(value);
-                                    },
-                                    onChanged: (value) {
-                                      print(value);
-                                    },
-                                    validator: (value) {
-                                      if (value!.isEmpty) {
-                                        return 'Password can\'t be empty';
-                                      } else
-                                        return null;
-                                    },
-                                    cursorColor: c1,
-                                    style: const TextStyle(fontSize: 25),
-                                    decoration: InputDecoration(
-                                      prefixIcon: Icon(
-                                        Icons.lock_rounded,
-                                        color: c1,
-                                        size: 30,
-                                      ),
-                                      suffixIcon: IconButton(
-                                        onPressed: () {
-                                          setState(() {
-                                            passwordcheck = !passwordcheck;
-                                          });
-                                        },
-                                        icon: Icon(
-                                          passwordcheck
-                                              ? Icons.remove_red_eye
-                                              : Icons.visibility_off,
-                                          color: c1,
-                                          size: 30,
-                                        ),
-                                      ),
-                                      hintText: 'password',
-                                      border: InputBorder.none,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) => ForgetPasswordScreen(),
-                                          ));
-                                    },
-                                    child: const Text(
-                                      'Forget Password ?',
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w500,
-                                        color: Colors.blue,
-                                      ),
-                                    ),
-                                  ),
-                                ],
                               ),
                               Default_Button(
 
                                 onPressed: (){
-                                  cubit.UserLogin(
-                                      email: 'abdulmajeedsallam@gmail.com',
-                                      password: 'P@ssw0rd');
 
-                                  if (formkey.currentState!.validate()) {
-                                    print(emailcontroller.text);
-                                    print(passwordcontroller.text);
-                                  } else {
-
-                                    // Dio_Helper.PostData(
-                                    //     url: 'https://crudapi20240209215103.azurewebsites.net/api/Account/logins',
-                                    //     data: {
-                                    //       'email':'abdulmajeedsallam@gmail.com',
-                                    //       'password' : 'P@ssw0rd',
-                                    //     }).then((value) {
-                                    //       print(value.data);
-                                    // }).catchError((error){
-                                    //   print('error ${error.toString()}');
-                                    // });
 
 
 
                                     Navigator.push(
                                         context,
                                         MaterialPageRoute(
-                                          builder: (context) => Layout_Screen(),
+                                          builder: (context) => loginscreen(),
                                         ));
-                                  }
+
                                 },
-                                text:'Sign in',
+                                text:'Next',
                               ),
                               // Container(
                               //   width: double.infinity,
